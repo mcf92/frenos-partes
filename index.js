a8bfededcb19215a04d67b7c027312a857d5d990
camilaFarelo <camilafarelo92@gmail.com>
2018-08-05 14:12:58 -0500

    new sections:
    - desvares
    - slider
    - plataforma

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import './source/js/jquery';
+import './node_modules/owl.carousel/dist/owl.carousel.min.js';
 import 'is-in-viewport'
 import './source/js/navbar';
 import './source/js/banner';
--- source/js/banner.js
+++ source/js/banner.js
@@ -1,9 +1,33 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         if ($('.home-services_item').isOnScreen()) {
-            $('.service').addClass('slideInUp');
+          $('.service').addClass('slideInUp');
+        }
+
+        if($('.correctivo').isOnScreen()) {
+          $('.card.animated').addClass('slideInUp');
         }
     });
 
-    $('.descriptiono:in-viewport').css( 'background-color', 'red' );
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'slideOutDown',
+      animateIn: 'flipInX',
+      loop:true,
+      margin:10,
+      responsive:{
+        0:{
+          items:1
+        }
+      },
+      stagePadding:30,
+      smartSpeed:450
+    });
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
+    // Go to the next item
+    $('.customNextBtn').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+
 });
